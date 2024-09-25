7bb3e0a2d1ff5bd428775173843c0d08ae791c04
Md-ObyduRahaman <sojib.19991018@gmail.com>
2024-09-25 12:27:58 +0600

    introduce student service

---
+++ studenService/src/test/java/com/studenService/StudenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studenService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

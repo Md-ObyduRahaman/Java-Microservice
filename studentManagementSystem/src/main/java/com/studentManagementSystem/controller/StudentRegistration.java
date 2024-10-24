3222c02aa13ac1cfe673f50917b0aa81204020c8
Md-ObyduRahaman <sojib.19991018@gmail.com>
2024-10-24 17:32:51 +0600

    circuitbreakers test done

--- studentManagementSystem/src/main/java/com/studentManagementSystem/controller/StudentRegistration.java
+++ studentManagementSystem/src/main/java/com/studentManagementSystem/controller/StudentRegistration.java
@@ -6,6 +6,7 @@ import com.studentManagementSystem.entity.Student;
 import com.studentManagementSystem.exception.ResourceNotFoundException;
 import com.studentManagementSystem.service.ParentDetailsService;
 import com.studentManagementSystem.service.StudentService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -84,6 +85,7 @@ public class StudentRegistration {
     }
 
     @GetMapping("guardian/{studentId}")
+    @CircuitBreaker(name = "parentDetailsBreaker" , fallbackMethod = "parentDetailsFallback")
     public ResponseEntity<BaseResponse<Optional<Student>>> getStudentParentDetails(@PathVariable Integer studentId){
         BaseResponse<Optional<Student>> response;
         Optional<Student> student = studentService.getStudentById(studentId);
@@ -97,6 +99,29 @@ public class StudentRegistration {
             throw new ResourceNotFoundException("Student not found" );
         }
     }
+
+    public ResponseEntity<BaseResponse<Optional<Student>>> parentDetailsFallback(Integer studentId, Throwable throwable) {
+        BaseResponse<Optional<Student>> response;
+
+        // Fetch the student even if parentDetailsService fails
+        Optional<Student> student = studentService.getStudentById(studentId);
+        student.get().setParentDetails(null);
+
+        if (student.isPresent()) {
+            // Create a message indicating that the student was found, but parent details couldn't be retrieved
+            response = new BaseResponse<>(
+                    "Student found, but parent details are unavailable at the moment. Cause: " + throwable.getMessage(),
+                    student,
+                    HttpStatus.OK,
+                    ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString()
+            );
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            // In case student itself is not found, still throw the ResourceNotFoundException
+            throw new ResourceNotFoundException("Student not found");
+        }
+    }
+
 
     @PostMapping("/addParent")
     public ResponseEntity<BaseResponse<ParentDetails>> addParent(@RequestBody ParentDetails Parent){
